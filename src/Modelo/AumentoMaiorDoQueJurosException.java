9f5c7a04cf69ef060dd075d97d0a9e51b53ca58e
Lucas Schneider <lcordeiro183@gmail.com>
2025-07-03 20:47:06 -0300

    Adicionado tratamento de erros.

---
+++ src/Modelo/AumentoMaiorDoQueJurosException.java
@@ -0,0 +1,7 @@
+package Modelo;
+
+public class AumentoMaiorDoQueJurosException extends Exception{
+    public AumentoMaiorDoQueJurosException (String mensagem) {
+        super(mensagem);
+    }
+}
